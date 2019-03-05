4ad6a7fcf888176e09e772569b9b3e41be51c363
fuziwang <2622860598@qq.com>
2019-03-05 16:19:27 +0800

    eslint grunt

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function(grunt){
+  grunt.initConfig({
+    eslint:{
+      options: {
+        configFile: '.eslintrc.json'    
+      },
+      target: ['rectangle.js']
+
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-eslint');
+
+  grunt.registerTask('default',['eslint']);
+};
