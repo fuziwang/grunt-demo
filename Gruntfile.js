edc938973368355c01f994f19acfc0fb6facfcd6
fuziwang <2622860598@qq.com>
2019-03-12 14:48:57 +0800

    uglify

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt){
+  grunt.initConfig({
+    uglify: {
+      release: {
+        files: {
+          'dist/rectangle.min.js': ['./rectangle.js'],
+          'dist/calc.min.js':['./calc.js']
+        }
+      }             
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default',['uglify']);
+};
