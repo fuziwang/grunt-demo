1e6581a81247488fe0a790371252686d4988e306
fuziwang <2622860598@qq.com>
2019-03-12 14:36:02 +0800

    htmlmin

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt){
+  grunt.initConfig({
+    htmlmin:{
+      options:{
+        collapseWhitespace: true,
+        preserveLineBreaks: false
+      },
+      dev: {
+        files: {
+          'dist/index.html': './index.html'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+
+  grunt.registerTask('default',['htmlmin']);
+};
